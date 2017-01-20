a93d8524a1c97a903b1ca35737ec7bb385c1affd
Agustin Malo <amalo@Agustins-MacBook-Pro.local>
2017-01-20 16:42:28 -0500

    Added djangoRest functionality

--- piggy/posts/views.py
+++ piggy/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework import permissions
+from posts.models import Post
+from posts.serializers import PostSerializer
+
+# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    # this fetches all the rows of data in the Fish table
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
