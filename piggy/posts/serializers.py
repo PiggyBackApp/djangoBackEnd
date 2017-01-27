6f4f269094de5cbd1720de0c898bc190eeac79b7
Agustin Malo <agustinmalo99@gmail.com>
2017-01-27 14:20:48 -0500

    Embedded customUsers in users and changed time to EST

--- piggy/posts/serializers.py
+++ piggy/posts/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
-        fields = ('title', 'description', 'timePosted', 'postType', 'origin', 'destination', 'emptySeats', 'passengerCapacity', 'status')
+        fields = ('title', 'creator','description', 'timePosted', 'postType', 'origin', 'destination', 'emptySeats', 'passengerCapacity', 'status')
