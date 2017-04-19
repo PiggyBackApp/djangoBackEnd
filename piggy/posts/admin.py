47121216f2a1b852d7d999ddd8a7594475d7675f
Agustin Malo <agustinmalo99@gmail.com>
2017-04-18 23:44:20 -0400

    Added confirmed requests to the admin console

--- piggy/posts/admin.py
+++ piggy/posts/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from posts.models import Post
 from posts.models import Request
 from posts.models import Review
+from posts.models import ConfirmedRequest
 admin.site.register(Post)
 admin.site.register(Request)
 admin.site.register(Review)
+admin.site.register(ConfirmedRequest)
